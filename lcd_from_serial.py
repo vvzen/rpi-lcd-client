d0a7171f9f86dc495939118cd56b9f328ed6821c
Valerio Viperino <vv@Valerios-Air.eduroam.student>
2018-03-22 00:09:44 +0000

    wrapped all inside main

--- lcd_from_serial.py
+++ lcd_from_serial.py
@@ -14,8 +14,6 @@ lcd_backlight = 4
 lcd_columns   = 16
 lcd_rows      = 2
 
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
-
 # communication stuff
 ser = serial.Serial(
     port="/dev/ttyAMA0",
@@ -28,28 +26,35 @@ ser = serial.Serial(
 
 display_regex = r"d:\w+"
 
-lcd.clear()
-lcd.set_cursor(0, 0)
-lcd.message("pi")
-lcd.set_cursor(1, 0)
-lcd.message("listenin")
+def main():
 
-while True:
-    current_message = ser.readline()
-    #print current_message
-    print "received a message from the udoo: {}".format(current_message)
+    lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-    # if we get the ready message from the udoo
-    # we know the connection is established, so then just print it to the display
-    if current_message == "udooready":
-        lcd.clear()
-        lcd.set_cursor(0, 0)
-        lcd.message("udoo ready")
-        lcd.set_cursor(0, 1)
-        lcd.message("pi ready")
-    # if a message starts with d: , send this message to the lcd
-    elif re.match(display_regex, current_message):
-        lcd.clear()
-        lcd.set_cursor(0, 0)
-        lcd.message(current_message[2:])
-        time.sleep(0.5)
+    lcd.clear()
+    lcd.set_cursor(0, 0)
+    lcd.message("pi")
+    lcd.set_cursor(1, 0)
+    lcd.message("listenin")
+
+    while True:
+        current_message = ser.readline()
+        #print current_message
+        print "received a message from the udoo: {}".format(current_message)
+
+        # if we get the ready message from the udoo
+        # we know the connection is established, so then just print it to the display
+        if current_message == "udooready":
+            lcd.clear()
+            lcd.set_cursor(0, 0)
+            lcd.message("udoo ready")
+            lcd.set_cursor(0, 1)
+            lcd.message("pi ready")
+        # if a message starts with d: , send this message to the lcd
+        elif re.match(display_regex, current_message):
+            lcd.clear()
+            lcd.set_cursor(0, 0)
+            lcd.message(current_message[2:])
+            time.sleep(0.5)
+    
+if __name__ == "__main__":
+    main()
